1cc017353b2251d101f65de5b708b7b12f5be097
riccardoperra <riccardo.perra@icloud.com>
2024-10-06 14:06:07 +0200

    improve accordion styles

--- packages/app/src/ui/components/Icon.tsx
+++ packages/app/src/ui/components/Icon.tsx
@@ -1,5 +1,5 @@
 import {icon, type IconVariants} from './Icon.css';
-import {mergeClasses} from '../../../../../../codeui/packages/kit';
+import {mergeClasses} from '@codeui/kit';
 
 type IconName = 'plus' | 'delete' | 'close' | 'fit_screen';
 
